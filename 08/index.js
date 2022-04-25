b1f904d0c00071852cf0db10b65e99df5c6330bc
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-25 14:18:41 -0300

    exercicio 08 feito

---
+++ 08/index.js
@@ -0,0 +1,8 @@
+const raio = 3;
+const comprimento = 2 * Math.PI * raio;
+
+console.log(`O comprimento do círculo é ${comprimento}.`);
+
+const area = Math.PI * raio ** 2;
+
+console.log(`A área do círculo é de ${area}`);
